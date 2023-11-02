c01dce978f64dfda6c08383a12bad868467949da
Lincoln Gibson <lincolngibson7@gmail.com>
2023-11-02 19:44:12 -0300

    add paragraph

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>
+      Highly skilled full-stack developer dedicated to writing clean code,
+      crafting accessible designs, and excelling in remote work environments.
+      Expertise in building web applications shines through mastery of React and Ruby on Rails.
+      Communicates effectively, ensuring seamless collaboration with colleagues, clients,
+      and stakeholders. Demonstrates exceptional problem-solving skills,
+      tackling both technical challenges and project management complexities.
+    </p>
   </div>
 </template>
 
@@ -11,6 +18,9 @@
     display: flex;
     place-content: center;
     align-items: center;
+  }
+  .about p {
+    font-size: 1.5rem;
   }
 }
 </style>
