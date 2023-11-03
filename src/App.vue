8d57a12cc63de86f3a331b591a48ba90c381fed7
Lincoln Gibson <lincolngibson7@gmail.com>
2023-11-03 18:41:09 -0300

    Refactor the app component.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
+  <Navbar />
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/projects">Projects</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <HelloWorld msg="Hi, my name is Lincoln Gibson." title="I'm a full-stack developer" />
     </div>
   </header>
 
-  <RouterView />
+<!--  <RouterView />-->
 </template>
 
 <style scoped>
