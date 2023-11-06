7c809baa10be17d7c3c8c3eec7377343d2794c42
Lincoln Gibson <lincolngibson7@gmail.com>
2023-11-06 14:03:29 -0300

    Install and Import fontawesome package.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faJs, faVuejs } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faLinkedin, faGithub, faTwitter, faInstagram, faJs, faVuejs);
 
 import App from './App.vue'
 import router from './router'
