38e7ef42218ba6bd3b10e055ae85fdc33371b967
Lincoln Gibson <lincolngibson7@gmail.com>
2023-11-07 16:27:38 -0300

    Add about me.

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="about">
+  <h1>About</h1>
+  <p>
+    I am a <strong>Software Engineer</strong> with a passion for <strong>Web Development</strong> and <strong>Machine Learning</strong>.
+  </p>
+</div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 1.6rem;
+  position: relative;
+  top: -10px;
+}
+
+p {
+  font-size: 1.2rem;
+  text-align: start;
+}
+
+strong {
+  font-weight: 700;
+  font-size: large;
+  color: green;
+}
+@media (min-width: 1024px) {
+  p {
+    text-align: start;
+  }
+}
+
+@media (max-width: 1023px) {
+  .about {
+    margin-top: 50px;
+  }
+}
+</style>
